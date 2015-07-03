83b20d877ef7f510c964d41eb6fb9a3a0caad7c0
Jeremy Hull <jchull@gmail.com>
2015-07-03 15:31:22 -0600

    Added in parallax via HTML5 template import, got style and script loading

---
+++ app/web-components/denver_parallax/script.js
@@ -0,0 +1,32 @@
+(function(){
+  'use strict';
+
+  angular
+    .module('coderado')
+    .directive('scrollLimit', function ($window) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        var raw = element[0];
+        var limitElement = raw.getElementsByClassName('limit');
+        var limit = 0;
+        angular.element(element).bind('scroll', function () {
+          if(limitElement[0].getBoundingClientRect().top<40){
+            if(limit>0)
+              raw.scrollTop = limit;
+            else
+              limit = raw.scrollTop;
+
+
+          }
+          /*console.log(raw.scrollTop + raw.offsetHeight);
+           console.log(raw.scrollHeight);
+           if (raw.scrollTop + raw.offsetHeight > raw.scrollHeight) {
+           scope.$apply();
+           }*/
+        });
+      }
+    };
+  });
+}());
+
