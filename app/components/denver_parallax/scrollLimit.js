1864d051c35e386f530141cc4addfa471a2a794d
Jeremy Hull <jchull@gmail.com>
2015-07-09 20:25:46 -0600

    working on scroll limit. Safari is better now, FF does not load the js from the imported HTML template (poss webcomponentsjs issue?)

--- app/components/denver_parallax/scrollLimit.js
+++ app/components/denver_parallax/scrollLimit.js
@@ -2,21 +2,32 @@
  * Created by jchull on 7/6/15.
  */
 (function(){
+
+  var handleScroll = function (event) {
+    var limitElement = this.querySelector('.limit');
+      if(limitElement.getBoundingClientRect().top<1){
+        console.log(this.scrollTop);
+        if(limit>0) {
+          this.scrollTop = limit-1;
+        }
+        else {
+          limit = this.scrollTop;
+        }
+      }
+    else {
+        // anything to do if scrolling back the other way?
+      }
+
+  }
+
   var scrollArea = document.querySelector('div[scroll-limit]');
   var limit = 0;
 
   if(scrollArea) {
     scrollArea.addEventListener(
       'scroll',
-      function (event) {
-        // add debounce later
-        if(document.querySelector('.limit').getBoundingClientRect().top<1){
-          if(limit>0)
-            this.scrollTop = limit;
-          else
-            limit = this.scrollTop;
-        }
-      }
+     // debounce(handleScroll, 250)
+      handleScroll
     );
   }
 
