667b651d03e90b390e0a17fdc3a82a33f4febd1b
Jeremy Hull <jchull@gmail.com>
2015-07-12 13:26:26 -0600

    Moved back to import, safari is close enough for now

--- app/components/denver_parallax/scrollLimit.js
+++ app/components/denver_parallax/scrollLimit.js
@@ -3,31 +3,49 @@
  */
 (function(){
 
-  var handleScroll = function (event) {
-    var limitElement = this.querySelector('.limit');
-      if(limitElement.getBoundingClientRect().top<1){
-        if(limit>0) {
-          this.scrollTop = limit-1;
-        }
+  var handleScroll = function (limitElement) {
+    return function(event) {
+      // Not sure if these are helping
+      event.preventDefault();
+      event.stopPropagation();
+
+      if (limitElement.getBoundingClientRect().top < 1) {
+        var p = this;
+        if (limit < 1 || p.scrollTop < limit)
+          limit = p.scrollTop;
+
+        if (p.scroll)
+          p.scroll(0, limit);
         else {
-          limit = this.scrollTop;
+          p.scrollTop = limit - 1;
         }
+
       }
-    else {
+      else {
         // anything to do if scrolling back the other way?
       }
+    }
 
   }
 
   var scrollArea = document.querySelector('div[scroll-limit]');
+
   var limit = 0;
 
   if(scrollArea) {
+    var limitElement = scrollArea.querySelector('.limit');
+
     scrollArea.addEventListener(
       'scroll',
-     // debounce(handleScroll, 250)
-      handleScroll
+      handleScroll(limitElement)
     );
+    scrollArea.addEventListener('touchmove', function(evt) {
+      //In this case, the default behavior is scrolling the body, which
+      //would result in an overflow.  Since we don't want that, we preventDefault.
+      if(!evt._isScroller) {
+        evt.preventDefault()
+      }
+    })
   }
 
 })();
