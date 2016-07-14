18d027330728f2e1d7a3b31488ce29b5cecf2173
Jeremy Hull <jchull@gmail.com>
2016-07-14 11:19:34 -0600

    Safari had issues with angular.bootstrap() so moved back to ng-app for now

--- app/scripts/ng-boot.js
+++ app/scripts/ng-boot.js
@@ -6,17 +6,18 @@
   // require('../styles/main.css');
 
   // load the main app file
-  var appModule = require('../index');
+ // var appModule =
+  require('../index');
 
   // replaces ng-app="appName"
-  /* global document*/
-  /* eslint no-undef: "error"*/
-  angular.element(document).ready(function() {
-    /* eslint angular/log: 0 */
-    console.log("bootstrapping angular module: " + appModule.name);
-    angular.bootstrap(document, [appModule.name], {
-      // strictDi: true
-    });
-  });
+  /* xxxglobal document*/
+  /* xxxeslint no-undef: "error"*/
+  // angular.element(document).ready(function() {
+  //   /* eslint angular/log: 0 */
+  //   console.log("bootstrapping angular module: " + appModule.name);
+  //   angular.bootstrap(document, [appModule.name], {
+  //     // strictDi: true
+  //   });
+  // });
 
 })();
